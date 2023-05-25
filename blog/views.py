c963893c441062a10b1c72456df3356b2feb93bd
Noura Arafa <noura_arafa31@hotmail.com>
2023-05-25 13:25:01 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
 
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
